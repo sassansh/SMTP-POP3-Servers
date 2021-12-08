ed292f9f3aae58d499c25a2bc4da6c06693c8403
UBC Student <lanakash@pender.students.cs.ubc.ca>
2021-12-08 00:06:59 -0800

    Added Comments

--- mysmtpd.c
+++ mysmtpd.c
@@ -10,6 +10,7 @@
 
 #define MAX_LINE_LENGTH 1024
 
+// Hash code of recognized commands
 #define HELO 754
 #define EHLO 757
 #define MAIL 730
@@ -19,10 +20,10 @@
 #define VRFY 816
 #define NOOP 793
 #define QUIT 806
-
 #define EXPN 797
 #define HELP 758
 
+// Server states
 typedef enum {
   GREET_NEXT,
   MAIL_NEXT,
@@ -60,10 +61,9 @@ int main(int argc, char *argv[]) {
 }
 
 void handle_client(int fd) {
-  
+  // Initialize server for new client
   char recvbuf[MAX_LINE_LENGTH + 1];
   net_buffer_t nb = nb_create(fd, MAX_LINE_LENGTH);
-
   struct utsname my_uname;
   uname(&my_uname);
 
@@ -81,13 +81,16 @@ void handle_client(int fd) {
       continue;
     }
 
+    // Get command and hash
     char *command = strtok(line, " ");
     int hashed_command = hash_command(command);
+
     if  (hashed_command == HELP || hashed_command == EXPN) {
       send_formatted(fd, "502 Unsupported command: %s\r\n", command);
       continue;
     }
 
+    // Deligate command to appropriate handler
     switch (hashed_command) {
       case HELO:
         hello(fd, my_uname.nodename);
@@ -102,7 +105,7 @@ void handle_client(int fd) {
         recipient(fd);
         break;
       case DATA: 
-        if (data(fd, nb) == -1) goto quit;
+        data(fd, nb);
         break;
       case RSET: 
         state = MAIL_NEXT;
@@ -126,17 +129,22 @@ void handle_client(int fd) {
   nb_destroy(nb);
 }
 
+// Handles HELO and EHLO commands 
+// Sends appropriate response codes to client
 void hello(int fd, char *domain) {
   if (state == GREET_NEXT) {
+    // check for client domain 
     if (strtok(NULL, " ")) {
-    state = MAIL_NEXT;
-    send_formatted(fd, "250 %s\r\n", domain);
-    return;
-    } 
-    send_formatted(fd, "550 No domain given\r\n");
+      state = MAIL_NEXT;
+      send_formatted(fd, "250 %s\r\n", domain);
+    } else {
+      send_formatted(fd, "550 No domain given\r\n");
+    }
   }
 }
 
+// Handles MAIL command
+// Verifies reverse path is given in corrrect format and sends appropriate response codes to client
 void mail(int fd) {
   // check that server was greeted and that no other mail transaction is in process
   if (state != MAIL_NEXT) {
@@ -145,11 +153,12 @@ void mail(int fd) {
   }
 
   char *param = strtok(NULL, " ");
-  // Error, no parameter 
+  // Error if no parameter 
   if (!param) {
     send_formatted(fd, "501 No parameter found\r\n"); 
     return;
   }
+
   // Check that reverse-path is specified with correct prefix
   if (strncasecmp(param, "FROM:<", 4)  != 0 || param[strlen(param) - 1] !='>') {
     send_formatted(fd, "501 Unsupported parameter\r\n"); 
@@ -157,11 +166,15 @@ void mail(int fd) {
   }
 
   state = RCPT_NEXT;
+  // clear and initialize mail transaction
   destroy_user_list(forward_paths);
   forward_paths = create_user_list();
+
   send_formatted(fd, "250 OK\r\n");
 }
 
+// Handles RCPT command
+// Verifies a valid user is given in the corrrect format and sends appropriate response codes to client
 void recipient(int fd) {
   if (state != RCPT_NEXT && state != DATA_NEXT) {
     send_formatted(fd, "503 Bad sequence of commands\r\n");
@@ -169,17 +182,19 @@ void recipient(int fd) {
   }
 
   char *param = strtok(NULL, " ");
-  // Error, no params found
+  // Error if no params found
   if (!param) {
     send_formatted(fd, "501 No parameters found\r\n");
     return;;
   }
+
   // Check that forward-path is specified with correct prefix and brackets
   if (strncasecmp(param, "TO:<", 4) != 0 || param[strlen(param) - 1] !='>') {
     send_formatted(fd, "501 Unsupported parameter\r\n");
     return;
   }
 
+  // Parse user from parameter
   int user_length = strlen(param) - 5;
   char user[user_length + 1];
   user[user_length] = '\0';
@@ -195,18 +210,21 @@ void recipient(int fd) {
   } 
 }
 
-// returns 1 if process is successful, returns -1 if client closed connection
+// Handles DATA command
+// Recieves mail transaction contents, saves to recipient(s)'s mailbox, and sends appropriate response codes to client
 void data(int fd, net_buffer_t nb) {
   if (state != DATA_NEXT) {
-    send_formatted(fd, "503 not greeted (Bad sequence of commands)\r\n");
+    send_formatted(fd, "503 Bad sequence of commands\r\n");
     return;
   }
 
   send_formatted(fd, "354 Start mail input; end with <CRLF>.<CRLF>\r\n");
   
+  // Initializa temporary mail file
   char temp_file_name[] = "Temp-XXXXXX";
   int temp_file = mkstemp(temp_file_name);
 
+  // Write client input into temporary file until terminator "." or client connection terminates
   char recvbuf[MAX_LINE_LENGTH + 1];
   int len = nb_read_line(nb, recvbuf);
   while (strcmp(recvbuf, ".\r\n") != 0 && len > 0) {
@@ -215,19 +233,25 @@ void data(int fd, net_buffer_t nb) {
   }
 
   save_user_mail(temp_file_name, forward_paths);
+  // Close temporary mail file
   unlink(temp_file_name);
   close(temp_file);
   state = MAIL_NEXT;
   send_formatted(fd, "250 OK\r\n");
 }
 
+// Handles VRFY command
+// Verifies user is a valid and sends appropriate response codes to client
 void verify(int fd) {
   char *user = strtok(NULL, " ");
-  // Error, no parameter 
+
+  // Error if no parameter 
   if (!user) {
     send_formatted(fd, "501 No parameter found\r\n"); 
     return;
   }
+
+  // Check if user is valid
   if (is_valid_user(user, NULL)) {
     send_formatted(fd, "250 <%s> is a valid user\r\n", user);
   } else {
@@ -235,6 +259,7 @@ void verify(int fd) {
   }
 }
 
+// Hashes string command into an integer code
 int hash_command(char *command) {
   if ( strlen(command) > 4 ) return -1;
   return ((int) command[0]) + 2*((int) command[1]) + 3*((int) command[2]) + 4*((int) command[3]);
